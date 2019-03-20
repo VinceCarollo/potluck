5b2557ec67f48e97b03c293e79919294244a26d1
Vince Carollo <vincecarollo@gmail.com>
2019-03-19 20:02:31 -0600

    Finish iteration 4

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -12,4 +12,24 @@ class Potluck
   def get_all_from_category(category)
     @dishes.find_all{|dish| dish.category == category}
   end
+
+  def menu
+    category_arr = @dishes.map{|dish| dish.category}
+    menu_hash = {}
+    category_arr.uniq.each do |category|
+      menu_hash[(category.to_s + 's').to_sym] = @dishes.find_all{|dish| dish.category == category}
+    end
+    menu_hash.each do |category, dishes|
+      dishes.map! do |dish|
+        dish.name
+      end
+      dishes.sort!
+    end
+    menu_hash
+  end
+
+  def ratio(category)
+    num_in_cat = @dishes.count{|dish| dish.category == category}
+    ((num_in_cat.to_f / @dishes.length) * 100).round(2)
+  end
 end
